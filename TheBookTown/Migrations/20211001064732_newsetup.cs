3b1da0a8faeb7edf38723e00622102857e1933fe
rohityadav7946 <rohityadav7946@gmail.com>
2022-04-04 16:57:12 +0530

    Complete Project

---
+++ TheBookTown/Migrations/20211001064732_newsetup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheBookTown.Migrations
+{
+    public partial class newsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Author = table.Column<string>(nullable: false),
+                    Genre = table.Column<int>(nullable: false),
+                    PhotoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Genre", "PhotoPath", "Title" },
+                values: new object[] { 1, "mark@gmail.com", 2, null, "Mark" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Genre", "PhotoPath", "Title" },
+                values: new object[] { 2, "marrry@gmail.com", 1, null, "Marry" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Department = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name", "PhotoPath" },
+                values: new object[] { 1, 2, "mark@gmail.com", "Mark", null });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name", "PhotoPath" },
+                values: new object[] { 2, 1, "marrry@gmail.com", "Marry", null });
+        }
+    }
+}
---
+++ TheBookTown/Migrations/20211002042832_addedbookpdf.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheBookTown.Migrations
+{
+    public partial class addedbookpdf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookPdfUrlPath",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Author", "Genre", "Title" },
+                values: new object[] { "James Clear", 8, "Atomic Habits" });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Author", "Genre", "Title" },
+                values: new object[] { "Paulo Coelho", 3, " The Alchemist" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookPdfUrlPath",
+                table: "Books");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Author", "Genre", "Title" },
+                values: new object[] { "mark@gmail.com", 2, "Mark" });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Author", "Genre", "Title" },
+                values: new object[] { "marrry@gmail.com", 1, "Marry" });
+        }
+    }
+}
